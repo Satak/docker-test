c0049b40c3c7a3424c0db54e14ec330d98a2e18c
satak <sami.koskivaara@fi.fujitsu.com>
2019-03-28 20:23:00 +0200

    Add templating

--- deploy_kubernetes.ps1
+++ deploy_kubernetes.ps1
@@ -1,5 +1,5 @@
 $files = Get-ChildItem -Recurse -Filter *.yaml | Where-Object {$_.Directory.Name -eq 'kube'}
 
 foreach($file in $files) {
-    kubectl apply -f $file.FullName
+    (Get-Content $file.FullName -Raw) -replace '{{appName}}', $file.Directory.Parent.Name | kubectl apply -f -
 }
