c2f90dc6cf815234c50683f38739063bbb6c2b41
satak <sami.koskivaara@fi.fujitsu.com>
2019-03-30 17:49:56 +0200

    Templating

--- build_all.ps1
+++ build_all.ps1
@@ -1,16 +1,38 @@
 param(
-    [string]$RootFolder = './services',
-    [string]$Version = 'latest',
-    [string]$DockerHubUser = 'satak'
+    [string]$RootFolder = './services'
 )
+Import-Module powershell-yaml
+
+$globalValuesContent = Get-Content -Path './kube/values.yaml' 
+$globalValues = ConvertFrom-Yaml -Yaml $globalValuesContent
+$globalValues.GetEnumerator() | ForEach-Object {Set-Variable -Name $_.Name -Value $_.Value}
 
 $srvFolders = Get-ChildItem -Path $RootFolder -Directory
 
 foreach($folder in $srvFolders) {
-    $tag = "$($DockerHubUser)/$($folder.Name):$($Version)"
-    docker kill $folder.Name
-    docker rm $folder.Name
-    docker rmi $folder.Name
-    docker build -t $tag $Folder
-    docker push $tag
+    Remove-Variable appName
+    $dockerfilePath = Join-Path -Path $folder.FullName -ChildPath 'Dockerfile'
+    if(Test-Path -Path $dockerfilePath -eq $false) {
+        continue
+    }
+    $valuesPath = Join-Path -Path $folder.FullName -ChildPath '/kube/values.yaml'
+    if(Test-Path $valuesPath -eq $false) {
+        continue
+    }
+    $valuesContent = Get-Content -Path $valuesPath
+    $values = ConvertFrom-Yaml -Yaml $valuesContent
+    $values.GetEnumerator() | ForEach-Object {Set-Variable -Name $_.key -Value $_.Value}
+
+    # docker
+    if($appName) {
+        $tag = "$($repository)/$($appName):$($version)"
+        docker kill $appName
+        docker rm $appName
+        docker rmi $appName
+        docker build -t $tag $Folder
+        docker push $tag
+    }
+
+    # kubernetes
+
 }
--- deploy_kube_context.ps1
+++ deploy_kube_context.ps1
@@ -1,4 +1,17 @@
-$template = Get-Content -Path ./kube/configMap.yaml -Raw
-$name = 'Bob'
-$version = 'latest'
-$ExecutionContext.InvokeCommand.ExpandString($template) | kubectl apply -f -
+Import-Module powershell-yaml
+
+$files = Get-ChildItem -Recurse -Filter *.yaml | Where-Object {$_.Directory.Name -eq 'kube' -and $_.name.ToLower() -ne 'values.yaml'}
+
+foreach($file in $files) {
+    $values = $null
+    $template = Get-Content -Path $file.FullName -Raw
+    $valuesPath = Join-Path -Path $file.Directory -ChildPath 'values.yaml'
+    if(Test-Path $valuesPath) {
+        $valuesContent = Get-Content -Path $valuesPath -ErrorAction SilentlyContinue
+        $values = ConvertFrom-Yaml -Yaml $valuesContent
+        $values.GetEnumerator() | ForEach-Object {Set-Variable -Name $_.key -Value $_.Value}
+    }
+
+    # $ExecutionContext.InvokeCommand.ExpandString($template)
+    # | kubectl apply -f -
+}
